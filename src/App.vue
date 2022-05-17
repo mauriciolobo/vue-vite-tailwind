b93fb2525baf7a9e476c1b62e99e2c5057667197
Mauricio Lobo <mauriciolobo@gmail.com>
2022-05-17 15:09:05 -0400

    Genesis

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+<div class="h-screen bg-gradient-to-r from-cyan-500 to-blue-500 flex flex-col items-center justify-center">
+  <h1 class="text-6xl font-bold text-cyan-100 font-serif">
+    Initial Template!
+  </h1>
+  <p class="font-extrabold leading-10 text-xl text-cyan-200 tracking-wider">
+    This is where everything begins...
+  </p>
+</div>
+</template>
+
+<style>
+</style>
